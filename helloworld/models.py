234547fe0f0f7f9d2dd17f3297c8d2a6ce079dc3
olddatawalrus <olddatawalrus@users.noreply.github.com>
2018-02-09 23:26:56 -0800

    Update models.py

--- helloworld/models.py
+++ helloworld/models.py
@@ -1,5 +1,28 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    author_img_path = models.CharField(max_length=255)
+    author_name = models.CharField(max_length=255)
+    post_date = models.CharField(max_length=255)
+    post_link = models.CharField(max_length=255)
+    post_title = models.CharField(max_length=255)
+    preview_img_path = models.CharField(max_length=255)
+    preview_text = models.CharField(max_length=255)
+    
+    
+def get_Post(self):
+   return ('post_preview', (),
+        {
+           'author_img_path': self.author_img_path,
+           'author_name': self.author_name,
+           'post_date': self.post_date,
+           'post_link': self.post_link,
+           'post_title': self.post_title,
+           'preview_img_path': self.preview_img_path,
+           'preview_text': self.preview_text,
+        })
