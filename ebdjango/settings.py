2a7a2ec08420d33f9cb20c37e5c0bea418ae79ea
olddatawalrus <olddatawalrus@users.noreply.github.com>
2018-02-09 16:47:01 -0800

    Update settings.py

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -112,12 +112,12 @@ WSGI_APPLICATION = 'ebdjango.wsgi.application'
 #         }
 #     }
     
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
