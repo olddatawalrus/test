958e9dc664cd4a1f9d0d2801036abedb795cb90c
olddatawalrus <olddatawalrus@users.noreply.github.com>
2018-02-11 12:41:47 -0800

    Update settings.py

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -85,7 +85,7 @@ if 'RDS_HOSTNAME' in os.environ:
             'ENGINE': 'django.db.backends.mysql',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': 'goofylou',
+            'PASSWORD': os.environ['RDS_PASSWORD'],
             'HOST': os.environ['RDS_HOSTNAME'],
             'PORT': os.environ['RDS_PORT'],
         }
