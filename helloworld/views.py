5e9813b311f9bb28fab607fba2ccb40ec5c19d05
olddatawalrus <olddatawalrus@users.noreply.github.com>
2018-02-11 13:15:41 -0800

    Update views.py

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,13 +1,13 @@
  # helloworld/views.py
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from post.models import Post
+from helloworld.models import Post
 
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
    
-    def post(request):
-      data = Post.objects.all()
-      return TemplateResponse(request,'helloworld/index.html',{ "data" : data } )
+def post(request):
+  data = Post.objects.all()
+  return TemplateResponse(request,'helloworld/index.html',{ "data" : data } )
