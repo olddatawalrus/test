b1edc05c583928b5af0a1b4fed13ed4cd2aa5bb4
olddatawalrus <olddatawalrus@users.noreply.github.com>
2018-02-11 20:15:24 -0800

    Update settings.py

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'helloworld',
+#     'helloworld',
+    'helloworld.apps.HelloworldConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
